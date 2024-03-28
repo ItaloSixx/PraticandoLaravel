b5464f477a63b95947717b27642fa30e10975cb3
ItaloSixx <anonimosbang@gmail.com>
2024-03-28 15:11:22 -0300

    continuando

--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use App\Models\Invoice;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class InvoiceFactory extends Factory
@@ -13,8 +15,13 @@ class InvoiceFactory extends Factory
      */
     public function definition()
     {
+        $paid = $this->faker->boolean();
         return [
-            //
+            'user_id' => User::factory(), 
+            'type' => $this->faker->randomElement(['A', 'B', 'C']),
+            'paid' => $paid,
+            'value' => $this->faker->numberBetween(1000, 2000),
+            'payment_date' => $paid ? $this->faker->dateTimeThisMonth() : null, 
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(40)->create();
+        \App\Models\Invoice::factory(40)->create();
+
     }
 }
